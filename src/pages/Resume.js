c881902477d094b18d505ce74818a06f50cd3b26
renburnett <renardburnett@gmail.com>
2021-01-21 14:39:20 -0800

    updated resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Grid, Container } from 'semantic-ui-react';
-import { Document, Page, pdfjs } from 'react-pdf';
-import ResumeJan20 from '../../src/FullstackJuly20.pdf';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Grid, Container } from "semantic-ui-react";
+import { Document, Page, pdfjs } from "react-pdf";
+import ResumeJan21 from "../../src/FullstackJan21.pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
 
 class Resume extends Component {
-
   state = {
     numPages: null,
     pageNumber: 1,
-  }
+  };
 
   componentDidMount() {
     pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -18,24 +17,26 @@ class Resume extends Component {
 
   onDocumentLoadSuccess = ({ numPages }) => {
     this.setState({ numPages });
-  }
+  };
 
   render() {
     const { pageNumber, numPages } = this.state;
 
     return (
-      <Grid stackable textAlign='center' verticalAlign='middle'>
+      <Grid stackable textAlign="center" verticalAlign="middle">
         <Grid.Column>
           <Grid.Row>
-            <Container as={Link} to={ResumeJan20} target="_blank" download>
+            <Container as={Link} to={ResumeJan21} target="_blank" download>
               <Document
-                file={ResumeJan20}
+                file={ResumeJan21}
                 onLoadSuccess={this.onDocumentLoadSuccess}
                 onLoadError={console.error}
               >
                 <Page pageNumber={pageNumber}></Page>
               </Document>
-              <p>Page {pageNumber} of {numPages}</p>
+              <p>
+                Page {pageNumber} of {numPages}
+              </p>
             </Container>
           </Grid.Row>
         </Grid.Column>
